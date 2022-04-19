51cc50589d57d6f2aeaf0bddd1568646014686d6
Dmitry <maximow-dimka@yandex.ru>
2022-04-19 16:14:37 +0300

    add notfound form

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Button, Stack, Typography } from '@mui/material';
+import Flex from '../components/UI/Flex';
 
 const NotFoundPage = () => {
-  return <div>ooops, page not found</div>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Flex
+        direction={'column'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        margin={'10%'}
+      >
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={3}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: 250,
+              fontWeight: 600,
+              textTransform: 'uppercase',
+              lineHeight: 0.7,
+              color: '#ffede1',
+            }}
+          >
+            404
+          </Typography>
+          <Typography
+            variant="h4"
+            textAlign={'center'}
+            sx={{
+              fontWeight: 500,
+            }}
+          >
+            Страница не найдена
+          </Typography>
+          <Typography variant="t" textAlign={'center'} mt={5}>
+            страница, которую вы просматриваете, не закрывается или была
+            перемещена.
+          </Typography>
+          <Button variant="contained" size="large">
+            Назад
+          </Button>
+        </Stack>
+      </Flex>
+    </React.Fragment>
+  );
 };
 
 export default NotFoundPage;
