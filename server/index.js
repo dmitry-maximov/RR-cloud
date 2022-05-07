64b9976be36d04e96de4a3a24c69a31f29977fbf
unknown <maximow-dimka@yandex.ru>
2022-05-07 22:29:54 +0300

    created connection to  db and added struct for users

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 6000;
 
@@ -8,8 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const start = () => {
+const start = async () => {
   try {
+    await sequelize.authenticate();
+    await sequelize.sync();
     app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
     console.log(e);
